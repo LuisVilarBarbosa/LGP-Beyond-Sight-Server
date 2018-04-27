24983d5928c2b4bc73e7696c67552d102ebd3c63
JP Mendonça <killghost15@gmail.com>
2018-04-27 17:36:17 +0100

    socket listening for events received from extension

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ import Pdf from "./pages/pdf";
 import {Switch, Route} from 'react-router-dom';
 import './css/Common.css';
 
+const portsocket = 8000;
+
+const server = express()
+  .listen(portsocket, () => console.log(`Listening on ${ portsocket }`));
+
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('message',(message)=>{
+    //message handling mby splitting ? mby different type according to each 
+
+
+  });
+});
+
 
 export default class App extends Component {
 
