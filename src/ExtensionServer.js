580c0e9126e7114a8b93a659921283d84a1ed49f
killghost15 <killghost15@gmail.com>
2018-05-04 16:07:54 +0100

    server waiting and receiving events

--- src/ExtensionServer.js
+++ src/ExtensionServer.js
@@ -1,21 +1,25 @@
-const express=require('express');
-
-const Websocket= require('ws'); 
-
-const portsocket = 8000;
 
 
-const server = express()
-  .listen(portsocket, () => console.log(`Listening on ${ portsocket }`));
+var net = require('net');
 
-const wss = new Websocket.Server({ server });
+// Create a TCP socket listener
+var s = net.Server(function (socket) {
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-  ws.on('message',(message)=>{
-    //message handling mby splitting ? mby different type according to each 
+    // Add the new client socket connection to the array of
+    // sockets
+
+    // 'data' is an event that means that a message was just sent by the 
+    // client application
+    socket.on('data', function (msg_sent) {
+        // Loop through all of our sockets and send the data
+       
+            // Write the msg sent by chat client
+            console.log(msg_sent.toString('utf8'));
+    });
+   
 
 
-  });
 });
+
+s.listen(8000);
+console.log('Node server at http://localhost:8000');
