1c02a42176fcfba310cbc448673e1a084dfcfed9
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2018-05-07 20:24:15 +0100

    File upload working
    Node is processing the data received when the socket is closed instead
    of on every TCP frame received.

--- src/ExtensionServer.js
+++ src/ExtensionServer.js
@@ -8,25 +8,33 @@ const files_url = files_dir.substr(3);
 
 // Create a TCP socket listener
 const s = net.Server(function (socket) {
+    socket.setEncoding('utf8');
+    let allData = "";
 
     // 'data' is an event that means that a message was just sent by the 
     // client application
-    socket.on('data', function (msg_sent) {
-        const last_command_byte = Math.min(msg_sent.indexOf("\0"), max_command_bytes - 1);
-        const command = msg_sent.slice(0, last_command_byte).toString('utf8');
+    socket.on('data', function (data) {
+        allData += data;
+    });
+
+    // This socket only processes the data when the socket is closed because
+    // 'data' is a TCP frame, not the full message.
+    socket.on('end', function(err) {
+        const last_command_byte = Math.min(allData.indexOf("\0"), max_command_bytes - 1);
+        const command = allData.slice(0, last_command_byte);
        
         if("Info" == command) {
-            const content = msg_sent.slice(max_command_bytes).toString('utf8');
+            const content = allData.slice(max_command_bytes);
             console.log(content);
         }
         else if("SENDING FILE" == command) {
             const content_pos = max_command_bytes + max_filename_bytes;
-            const last_filename_byte = Math.min(msg_sent.indexOf("\0", max_command_bytes), content_pos - 1);
-            const filename = msg_sent.slice(max_command_bytes, last_filename_byte).toString('utf8');
-            const content = msg_sent.slice(content_pos);
+            const last_filename_byte = Math.min(allData.indexOf("\0", max_command_bytes), content_pos - 1);
+            const filename = allData.slice(max_command_bytes, last_filename_byte);
+            const content = allData.slice(content_pos);
             const storedFilename = files_dir + filename;
             const storedFileURL = files_url + filename;
-            fs.writeFile(storedFilename, content, function(err) {
+            fs.writeFile(storedFilename, content, 'utf8', function(err) {
                 if(err) {
                     return console.error(err);
                 }
@@ -35,19 +43,19 @@ const s = net.Server(function (socket) {
             socket.write(storedFileURL);
         }
         else if("SLIDE_SHOW_BEGIN_EVENT" == command) {
-            const content = msg_sent.slice(max_command_bytes).toString('utf8');
+            const content = allData.slice(max_command_bytes);
             // Inform REACT
         }
         else if("SLIDE_SHOW_NEXT_SLIDE_EVENT" == command) {
-            const content = msg_sent.slice(max_command_bytes).toString('utf8');
+            const content = allData.slice(max_command_bytes);
             // Inform REACT
         }
         else if("SLIDE_SHOW_NEXT_BUILD_EVENT" == command) {
-            const content = msg_sent.slice(max_command_bytes).toString('utf8');
+            const content = allData.slice(max_command_bytes);
             // Inform REACT
         }
         else if("SLIDE_SHOW_END_EVENT" == command) {
-            const content = msg_sent.slice(max_command_bytes).toString('utf8');
+            const content = allData.slice(max_command_bytes);
             // Inform REACT
         }
         else
@@ -55,7 +63,6 @@ const s = net.Server(function (socket) {
     });
 
     socket.on('error', function(err) {
-        console.error(err);
     });
 });
 
