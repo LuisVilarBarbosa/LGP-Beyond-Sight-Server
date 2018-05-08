a59ea3bb681cc434e8ed5cb7dd429811a7ebcb66
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2018-05-08 17:22:35 +0100

    Using HTTP communication with PPT instead of TCP
    HTTP communication has the following advantages:
    - Receives the full message, not being necessary to append TCP chunks
    and wait for the connection to be closed to process the message;
    - Allows the reuse of the connection because the communication does not
    need to be closed for the data to be processed.
    - Provides the ability to use relative links instead of commands inside
    the body of the message.

--- src/ExtensionServer.js
+++ src/ExtensionServer.js
@@ -1,64 +1,52 @@
-const net = require('net');
+// Based on https://www.c-sharpcorner.com/article/socket-io-programming-with-example-in-node-js/
+var express = require('express');
+var app = express();
 const fs = require('fs');
 const port = 8000;
 const files_dir = "../uploaded_files/";
 const files_url = files_dir.substr(3);
 const delimiter = '\0';
 
-// Create a TCP socket listener
-const s = net.Server(function (socket) {
-    socket.setEncoding('utf8');
-    let allData = "";
-
-    // 'data' is an event that means that a message was just sent by the 
-    // client application
-    socket.on('data', function (data) {
-        allData += data;
-    });
-
-    // This socket only processes the data when the socket is closed because
-    // 'data' is a TCP frame, not the full message.
-    socket.on('end', function(err) {
-        const firstNull = allData.indexOf(delimiter);
-        const command = allData.slice(0, firstNull);
-        const payload = allData.slice(firstNull + 1);
-       
-        if("Info" == command) {
-            console.log(payload);
-        }
-        else if("SENDING FILE" == command) {
-            const secondNull = payload.indexOf(delimiter);
-            const filename = payload.slice(0, secondNull);
-            const fileContent = payload.slice(secondNull + 1);
-            const storedFilename = files_dir + filename;
-            const storedFileURL = files_url + filename;
-            fs.writeFile(storedFilename, fileContent, 'utf8', function(err) {
-                if(err) {
-                    return console.error(err);
-                }
-                console.log("The file '" + storedFilename + "' was saved!");
-            });
-            socket.write(storedFileURL);
-        }
-        else if("SLIDE_SHOW_BEGIN_EVENT" == command) {
-            // Inform REACT
-        }
-        else if("SLIDE_SHOW_NEXT_SLIDE_EVENT" == command) {
-            // Inform REACT
-        }
-        else if("SLIDE_SHOW_NEXT_BUILD_EVENT" == command) {
-            // Inform REACT
-        }
-        else if("SLIDE_SHOW_END_EVENT" == command) {
-            // Inform REACT
-        }
-        else
-            console.log("Unexpected command received: " + command);
-    });
-
-    socket.on('error', function(err) {
+app.put("/upload_file", function(request, response){
+    let body = [];
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        const nullIndex = body.indexOf(delimiter);
+        const filename = body.slice(0, nullIndex);
+        const fileContent = body.slice(nullIndex + 1);
+        const storedFilename = files_dir + filename;
+        const storedFileURL = files_url + filename;
+        fs.writeFile(storedFilename, fileContent, 'utf8', function(err) {
+            if(err) {
+                return console.error(err);
+            }
+            console.log("The file '" + storedFilename + "' was saved!");
+        });
+        response.writeHead(200,{'Content-Type':'text/html'});
+        response.write(storedFileURL);
+        response.end();
     });
 });
 
-s.listen(port);
-console.log('Node server running on http://localhost:' + port);
+
+app.post("/slide_show_begin_event", function(request, response){
+    response.end();
+});
+
+app.post("/slide_show_next_slide_event", function(request, response){
+    response.end();
+});
+
+app.post("/slide_show_next_build_event", function(request, response){
+    response.end();
+});
+
+app.post("/slide_show_end_event", function(request, response){
+    response.end();
+});
+
+app.listen(port, function() {
+    console.log((new Date().toUTCString()) + ': Node server running on http://localhost:' + port);
+});
